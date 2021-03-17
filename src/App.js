40f63655aaf79c98db6efb1de13a8c3e26a72169
Manish Sajwan <msoffice37@gmail.com>
2021-03-17 11:46:25 -0700

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div id="something-important">
       <h1>Adopt Me</h1>
-      <Pet name="Luna" animal="dog" breed="Havenese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
